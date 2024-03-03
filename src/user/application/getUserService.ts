92e50688c98bcdd0d1a4126202e224a5b72c2141
jerojash <javiereduardorh@gmail.com>
2024-03-03 14:55:53 -0400

    Add getUserService

---
+++ src/user/application/getUserService.ts
@@ -0,0 +1,15 @@
+import { Either } from "src/generics/Either";
+import { IUser } from "../domain/repository/IUser";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "../domain/User";
+
+export class getUserService<T>{
+    private UserRepository: IUser<T>;
+    constructor(repo: IUser<T>) {
+        this.UserRepository = repo;
+    }
+    async execute(id: string): Promise<Either<Error,T>>{
+        return this.UserRepository.getUsers(id)
+    }
+
+}

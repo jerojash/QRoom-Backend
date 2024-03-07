d0f6e82b52b18c88aef6cbf39b86c72ea62929b7
jerojash <javiereduardorh@gmail.com>
2024-03-06 20:55:52 -0400

    Add infrastructure resource

---
+++ src/room/infrastructure/room.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { RoomService } from './room.adapter';
+import { CreateRoomDto } from '../application/dto/create-room.dto';
+import { UpdateRoomDto } from '../application/dto/update-room.dto';
+
+@Controller('room')
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+
+  @Post()
+  create(@Body() createRoomDto: CreateRoomDto) {
+    return this.roomService.create(createRoomDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.roomService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.roomService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
+    return this.roomService.update(+id, updateRoomDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.roomService.remove(+id);
+  }
+}

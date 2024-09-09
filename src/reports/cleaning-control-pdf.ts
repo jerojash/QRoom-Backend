c93e564fdd838b30345f52323cf99b844f91237a
jerojash <javiereduardorh@gmail.com>
2024-09-08 21:49:07 -0400

    Add const

--- src/reports/cleaning-control-pdf.ts
+++ src/reports/cleaning-control-pdf.ts
@@ -151,6 +151,8 @@ function createTile(title: string, subTitle: string): Content[] {
 }
 
 function createTableDashboard(area: AreaEntity[], next: boolean = true): Content {
+  const url = `${process.env.URL}/api/cleaning-action/pdf`
+  console.log('url: ', url);
   return {
     layout: 'customLayout01', // optional
     table: {
@@ -191,7 +193,7 @@ function createTableDashboard(area: AreaEntity[], next: boolean = true): Content
           room.actions[0] ? [
             {
               text: room.name,
-              link: `${process.env.URL}/api/cleaning-action/pdf/${room.name}`
+              link: `${url}/${room.name}`
             },
             // If it has been more than 24 hours
             (new Date().getTime() - room.actions[0].initial_time_hk.getTime()) > 24 * 60 * 60 * 1000

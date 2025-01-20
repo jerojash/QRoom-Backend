b58dd2ea93284d724e009a1680155f57e6df280a
jerojash <javiereduardorh04@gmail.com>
2025-01-20 00:18:07 -0400

    Refactor service

--- src/cleaningCheck/application/createCleaningCheckService.ts
+++ src/cleaningCheck/application/createCleaningCheckService.ts
@@ -1,20 +1,36 @@
-import { Either } from "src/generics/Either";
-import { ICleaningCheck } from "../domain/repository/ICleaningCheck";
-import { CreateCleaningCheckDto } from "./dto/create-cleaning-check.dto";
-import { CleaningCheck } from "../domain/cleaningCheck";
+import { Either } from 'src/generics/Either';
+import { ICleaningCheck } from '../domain/repository/ICleaningCheck';
+import { CreateCleaningCheckDto } from './dto/create-cleaning-check.dto';
+import { CleaningCheck } from '../domain/cleaningCheck';
 
-export class createCleaningCheckService<T>{
-    private CleaningCheckRepository: ICleaningCheck<T>;
-    constructor(repo: ICleaningCheck<T>) {
-        this.CleaningCheckRepository = repo;
-    }
-    async execute(dto: CreateCleaningCheckDto ): Promise<Either<Error,T>>{
+export class createCleaningCheckService<T> {
+  private CleaningCheckRepository: ICleaningCheck<T>;
+  constructor(repo: ICleaningCheck<T>) {
+    this.CleaningCheckRepository = repo;
+  }
+  async execute(dto: CreateCleaningCheckDto): Promise<Either<Error, T>> {
+    let cleaningCheck: CleaningCheck;
+    let result;
+    // const cleaningCheck = CleaningCheck.create(dto.name, dto.order,
+    //     dto.type_id, dto.id_parent_task);
 
-        const cleaningCheck = CleaningCheck.create(dto.name, dto.order,
-            dto.type_id, dto.id_parent_task);
+    await Promise.all(
+      dto.name.map(async (name, index) => {
+        console.log('value: ', name);
+        cleaningCheck = CleaningCheck.create(
+          name,
+          dto.order + index,
+          dto.type_id ?? null,
+          dto.id_parent_task ?? null
+        );
+        console.log('\n\nCHECK: ', cleaningCheck);
+        result =
+          this.CleaningCheckRepository.createCleaningCheck(cleaningCheck);
+      })
+    );
 
-        let result = this.CleaningCheckRepository.createCleaningCheck(cleaningCheck);
-        return result
-    }
-
+    // let result =
+    //   this.CleaningCheckRepository.createCleaningCheck(cleaningCheck);
+    return result;
+  }
 }
--- src/cleaningCheck/application/dto/create-cleaning-check.dto.ts
+++ src/cleaningCheck/application/dto/create-cleaning-check.dto.ts
@@ -1,19 +1,24 @@
-import { IsNumber, IsOptional, IsString, IsUUID, MinLength } from "class-validator";
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCleaningCheckDto {
+  @IsArray()
+  public name: string[];
 
-    @IsString()
-    @MinLength(1)
-    public name: string 
+  @IsOptional()
+  @IsUUID()
+  public type_id?: string;
 
-    @IsOptional()
-    @IsUUID()
-    public type_id: string
+  @IsNumber()
+  public order: number;
 
-    @IsNumber()
-    public order: number 
-
-    @IsOptional()
-    @IsUUID()
-    public id_parent_task?: string
+  @IsOptional()
+  @IsUUID()
+  public id_parent_task?: string;
 }
